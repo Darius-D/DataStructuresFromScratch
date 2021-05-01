9a39f4fde2e63a6900cb16a8b19c80536e3373e6
Darius <ddubose93@gmail.com>
2021-04-30 22:47:55 -0700

    Add project files.

---
+++ DataStructures/LinkedListPractice.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class LinkedListPractice
+    {
+        public PNode Head;
+
+        public LinkedListPractice()
+        {
+            Head = null;
+        }
+
+        public void AddToEnd(int value)
+        {
+            if (Head == null)
+            {
+                Head = new PNode(value);
+            }
+            else
+            {
+                Head.AddPNode(value);
+            }
+        }
+
+        public void AddToStart(int value)
+        {
+            if (Head != null)
+            {
+                var temp = new PNode(value);
+                temp.Next = Head;
+
+                Head = temp;
+            }
+        }
+
+        public void PrintList()
+        {
+            if (Head != null)
+            {
+                Head.Print();
+            }
+        }
+
+        public void Delete(int value)
+        {
+            var currentNode = Head;
+
+            if (Head != null)
+            {
+                if (Head.Value == value)
+                {
+                    Head = Head.Next;
+                }
+
+                while (currentNode.Next.Value != value)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                currentNode.Next = currentNode.Next.Next;
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            var currentNode = Head;
+
+            if (Head == null)
+            {
+                return false;
+            }
+            while (currentNode.Value != value && currentNode.Next != null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            if (currentNode.Value == value)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class PNode
+    {
+        public int Value;
+
+        public PNode Next;
+
+        public PNode(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+
+        public void AddPNode( int data)
+        {
+            if (Next == null)
+            {
+                Next = new PNode(data);
+            }
+            else
+            {
+                Next.AddPNode(data);
+            }
+        }
+
+        public void Print()
+        {
+            
+            Console.Write($"|{Value}|->");
+
+            if (Next != null)
+            {
+                Next.Print();
+            }
+                
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          var test = new LinkedListPractice();
+          test.AddToEnd(1);
+          test.AddToEnd(2);
+          test.PrintList();
+          Console.WriteLine("\n");
+          test.AddToStart(0);
+          test.PrintList();
+
+          Console.WriteLine("\n");
+          test.AddToEnd(3);
+          test.AddToEnd(4);
+          test.AddToEnd(5);
+          test.AddToEnd(6);
+          test.AddToEnd(7);
+
+
+
+            Console.WriteLine(test.Contains(4));
+
+        }
+    }
+}
